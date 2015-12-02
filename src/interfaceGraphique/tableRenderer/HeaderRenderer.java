d9035b079379a8c4c94663e452deb440e9965e1e
Faustine <faustine.maffre@gmail.com>
2015-12-02 16:29:08 +0100

    Ajout de doc dans le package 'interfaceGraphique.tableRenderer'

--- src/interfaceGraphique/tableRenderer/HeaderRenderer.java
+++ src/interfaceGraphique/tableRenderer/HeaderRenderer.java
@@ -8,10 +8,18 @@ import javax.swing.border.MatteBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.JTableHeader;
 
+/**
+ * Gere le rendu de la premiere ligne de chaque tableau de l'IHM, qui affiche 
+ * le nom des colonnes. 
+ *
+ */
 public class HeaderRenderer extends DefaultTableCellRenderer {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Cree un renderer pour la premiere ligne des tableaux. 
+	 */
 	public HeaderRenderer() {
 		setHorizontalAlignment(CENTER);
 		setHorizontalTextPosition(CENTER);
@@ -22,15 +30,19 @@ public class HeaderRenderer extends DefaultTableCellRenderer {
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value,
 			boolean isSelected, boolean hasFocus, int row, int column) {
-		super.getTableCellRendererComponent(table, value,
-				isSelected, hasFocus, row, column);
+		
+		super.getTableCellRendererComponent(table, value, isSelected, hasFocus, 
+				row, column);
+		
 		JTableHeader tableHeader = table.getTableHeader();
+		
 		if (tableHeader != null) {
 			setForeground(tableHeader.getForeground());
 		}
 
 		Border border = new MatteBorder(0, 1, 0, 1, tableHeader.getForeground());
 		setBorder(border);
+		
 		return this;
 	}
 
--- src/interfaceGraphique/tableRenderer/NormalRenderer.java
+++ src/interfaceGraphique/tableRenderer/NormalRenderer.java
@@ -13,19 +13,28 @@ import javax.swing.border.Border;
 import javax.swing.border.MatteBorder;
 import javax.swing.table.TableCellRenderer;
 
+/**
+ * Gere le rendu des cases des tableaux de l'IHM. 
+ *
+ */
 public class NormalRenderer extends JLabel implements TableCellRenderer {
 
 	private static final long serialVersionUID = 1L;
+	
 	/**
-	 * Couleur de fond des cellules
+	 * Couleur de fond des cellules.
 	 */
 	private Color cellBackground;
 	/**
-	 * Couleur du texte des cellules
+	 * Couleur du texte des cellules.
 	 */
 	private Color cellForeground;
 	
-	
+	/**
+	 * Cree le renderer pour les cellules des tableaux. 
+	 * @param cellBackground couleur de fond des cellules
+	 * @param cellForeground couleur du texte des cellules
+	 */
 	public NormalRenderer(Color cellBackground, Color cellForeground) {
 		Border border = new MatteBorder(0, 0, 3, 0, Color.WHITE);
 		setBorder(border);
@@ -34,7 +43,6 @@ public class NormalRenderer extends JLabel implements TableCellRenderer {
 		this.cellBackground = cellBackground;
 		this.cellForeground = cellForeground;
 	}
-
 
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value,
@@ -47,8 +55,8 @@ public class NormalRenderer extends JLabel implements TableCellRenderer {
 		setForeground(cellForeground);
 		setBackground(cellBackground);
 		
+		// tableau des personnages pendant la partie
 		if (table.getModel() instanceof PersonnageTableModel) {
-			
 			PersonnageTableModel model = (PersonnageTableModel) table.getModel();
 			
 			if (!model.isConnected(row)){
@@ -60,7 +68,8 @@ public class NormalRenderer extends JLabel implements TableCellRenderer {
 			}
 		}
 		
-		if (table.getModel() instanceof ObjetTableModel){
+		// tableau des objets
+		if (table.getModel() instanceof ObjetTableModel) {
 			ObjetTableModel model = (ObjetTableModel) table.getModel();
 
 			if (isSelected || model.isSelected(row) || column == model.getIndexNom()) {
@@ -68,8 +77,10 @@ public class NormalRenderer extends JLabel implements TableCellRenderer {
 			}
 		}
 		
-		if (table.getModel() instanceof ClassementTableModel){
+		// classement des personnages a la fin de la partie
+		if (table.getModel() instanceof ClassementTableModel) {
 			ClassementTableModel model = (ClassementTableModel) table.getModel();
+			
 			if (column == model.getIndexNom()) {
 				setBackground(model.getColor(row));
 			}
