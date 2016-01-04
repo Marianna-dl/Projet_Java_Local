4247fdb4c6a41b82e2eec45874a726b979b5822f
Marianna <marianna95220@gmail.com>
2016-01-04 16:26:35 +0100

    rectif potion

--- src/lanceur/LancePotion.java
+++ src/lanceur/LancePotion.java
@@ -62,7 +62,7 @@ public class LancePotion {
 			// caracteristiques de la potion
 			HashMap<Caracteristique, Integer> caractsPotion = new HashMap<Caracteristique, Integer>();
 			caractsPotion.put(Caracteristique.VIE, 100);
-			//caractsPotion.put(Caracteristique.VIE, Calculs.nombreAleatoire(-100, 100));
+			caractsPotion.put(Caracteristique.VIE, Calculs.nombreAleatoire(-100, 100));
 			caractsPotion.put(Caracteristique.FORCE, Calculs.nombreAleatoire(-100, 100));
 			caractsPotion.put(Caracteristique.INITIATIVE, Calculs.nombreAleatoire(-100, 100));
 			
